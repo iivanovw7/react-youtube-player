b84d187a25bb5427d9ef2a7901652d41c214bd70
iivanovw7 <9iiiuk2@gmail.com>
2018-04-27 19:12:12 +0300

    added build in package

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,28 @@
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './',
-      port: 3333
-  }
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['react', 'es2015', 'stage-1']
+            }
+        }, {test: /\.css$/, loader: "style-loader!css-loader"},
+            {test: /\.(eot|svg|ttf|woff|woff2)$/, loader: 'file-loader',}]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './',
+        port: 3333
+    }
 };
