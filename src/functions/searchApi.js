6b88aefd0b48dd7db0c07c4b4a002e1005feb04c
iivanovw7 <9iiiuk2@gmail.com>
2018-05-08 20:12:55 +0300

    changed search api file

--- src/functions/searchApi.js
+++ src/functions/searchApi.js
@@ -7,7 +7,42 @@ export function fetchVideos(options, callback) {
         throw new Error('Youtube Search expected key, received undefined');
     }
 
+    let inputMaxResults = 1;
+
+        if (inputMaxResults <= 0) {
+            inputMaxResults = 1;
+        } else if (inputMaxResults >= 50) {
+            inputMaxResults= 50;
+        } else {
+            inputMaxResults = options.maxResults;
+        }
+
+
+
     let params = {
+        part: 'snippet',
+        key: options.key,
+        q: options.term,
+        type: 'video',
+        maxResults: inputMaxResults
+    };
+
+
+    axios.get(ROOT_URL, {params: params})
+        .then(function (response) {
+            if (callback) {
+                callback(response.data.items);
+            }
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+};
+
+/*
+
+
+let params = {
         part: 'snippet',
         key: options.key,
         q: options.term,
@@ -34,16 +69,5 @@ export function fetchVideos(options, callback) {
     }
 
 
-    axios.get(ROOT_URL, {params: params})
-        .then(function (response) {
-            if (callback) {
-                callback(response.data.items);
-            }
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
-};
-
-
+ */
 
